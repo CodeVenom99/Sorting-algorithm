48ee85210caa979a41065afcb9746d81ef0aea66
CodeVenom99 <1060401004@qq.com>
2020-03-01 17:23:10 +0800

    统一从大到小排序

--- 常用排序算法/源.cpp
+++ 常用排序算法/源.cpp
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <ctime>
-void maopao_sort(int* p, int len);//冒泡排序
-void choice_sort(int* p, int len);//选择排序
-void Quick_sort(int* p, int L, int H);//快速排序
+void maopao_sort(int* p, int len);//冒泡排序，从大到小
+void choice_sort(int* p, int len);//选择排序，从大到小
+void Quick_sort(int* p, int L, int H);//快速排序，从大到小
 int find_pos(int* p, int L, int H);
 void show(int* p, int len);
 int main()
 {
-	int num[6] = { -2, 1, 0, -985, 4, -93 };
+	int num[6] = { 1,2,3,4,5,6};
 //	maopao_sort(num, sizeof(num) / sizeof(int));
 //	choice_sort(num, sizeof(num) / sizeof(int));
 	Quick_sort(num, 0,( sizeof(num) / sizeof(int) )- 1);
@@ -59,12 +59,12 @@ int find_pos(int* p, int L, int H)
 	int val = p[L];
 	while (L < H)
 	{
-		while (p[H] >= val&& L < H)
+		while (p[H] <= val&& L < H)
 		{
 			--H;
 		}
 		p[L] = p[H];
-		while (p[L] <= val&& L < H)
+		while (p[L] >= val&& L < H)
 		{
 			++L;
 		}
